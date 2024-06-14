26e9dc0e7b074ae837e92dd01473d382d2cd28e8
NishanSathsara <nishanksathsara@gmail.com>
2024-06-15 04:33:20 +0530

    create theme file

---
+++ src/shared/components/atoms/theme.ts
@@ -0,0 +1,32 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#cf9fff",
+    },
+    secondary: {
+      main: "#E19133",
+    },
+    text: {
+      primary: "#191b33",
+      secondary: "#cf9fff",
+    },
+  },
+  typography: {
+    fontFamily: "'Lexend Deca',sans-serif, 'Poppins'",
+    fontSize: 14
+
+  },
+  components:{
+    MuiButton:{
+        styleOverrides:{
+            root:{
+                color:"#fff",
+                fontWeight: "700",
+                fontSize:"1rem",
+            }
+        }
+    }
+  },
+});
