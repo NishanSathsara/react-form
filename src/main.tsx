5fe21c044f0bd160488f4f715f0089dbbdace2c1
NishanSathsara <nishanksathsara@gmail.com>
2024-06-15 04:32:46 +0530

    add the mui theme provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-import Home from './pages/Home/Home';
+import { ThemeProvider } from "@emotion/react";
+import Home from "./pages/Home/Home";
+import { theme } from "./shared/components/atoms/theme";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <Home />,
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+    <ThemeProvider theme={theme}>
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  </React.StrictMode>
+);
